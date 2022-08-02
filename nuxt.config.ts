ce3e695f765940e86b1db2d2b370a8f7c367d8cf
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-08-03 00:32:54 +0530

    remove unused plugins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,7 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ["vuetify/lib/styles/main.sass", "mdi/css/materialdesignicons.min.css"],
-  modules: ["@vueuse/nuxt"],
-  buildModules: ["@pinia/nuxt"],
+  modules: ["@pinia/nuxt", "@vueuse/nuxt"],
   ssr: false,
   build: {
     transpile: ["vuetify"],
