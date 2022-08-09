511d7b51b7876d191c5110203914a54687fc15a3
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-08-09 19:59:18 +0530

    hide fab on scroll

--- components/Fab.vue
+++ components/Fab.vue
@@ -4,6 +4,31 @@ defineProps<{
 }>();
 
 const active = ref<boolean>(false);
+const hidden = ref<boolean>(false);
+
+let last_known_scroll_position = 0;
+let ticking = false;
+const threshold = 50; // in pixels
+const onScroll = () => {
+  const dy = window.scrollY - last_known_scroll_position;
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function () {
+      active.value = false;
+      if (Math.abs(dy) > threshold) {
+        if (dy > 0) {
+          hidden.value = true;
+        } else {
+          hidden.value = false;
+        }
+      }
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+};
 
 function toggleActive() {
   active.value = !active.value;
@@ -12,26 +37,35 @@ function toggleActive() {
 function close() {
   active.value = false;
 }
+
+onMounted(() => {
+  window.addEventListener("scroll", onScroll);
+});
+onUnmounted(() => {
+  window.removeEventListener("scroll", onScroll);
+});
 </script>
 
 <template>
-  <div class="wrapper">
-    <transition name="fade">
-      <div v-if="active" class="actions">
-        <slot name="actions" :close="close"> </slot>
+  <transition name="fade">
+    <div class="wrapper" v-if="!hidden">
+      <transition name="fade">
+        <div v-if="active" class="actions">
+          <slot name="actions" :close="close"> </slot>
+        </div>
+      </transition>
+      <div class="fab" :class="{ active: active }">
+        <slot>
+          <v-btn
+            @click="toggleActive()"
+            :variant="active ? 'flat' : 'elevated'"
+            :icon="active ? 'mdi-close' : icon"
+          >
+          </v-btn>
+        </slot>
       </div>
-    </transition>
-    <div class="fab" :class="{ active: active }">
-      <slot>
-        <v-btn
-          @click="toggleActive()"
-          :variant="active ? 'flat' : 'elevated'"
-          :icon="active ? 'mdi-close' : icon"
-        >
-        </v-btn>
-      </slot>
     </div>
-  </div>
+  </transition>
 </template>
 
 <style scoped>
@@ -53,8 +87,7 @@ function close() {
 }
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.2s ease;
-  transition: transform 0.3s ease-out;
+  transition: opacity 0.2s ease-out, transform 0.3s ease-out;
 }
 
 .fade-enter-from,
