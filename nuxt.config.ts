32630dc7e51f600b25d68d579627e3fd45e89d40
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-08-11 22:54:41 +0530

    update workbox config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      script: [{ src: "/registerSW.js" }],
       link: [
         {
           rel: "icon",
--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,6 @@
 module.exports = {
-	globDirectory: '.output/public',
-	globPatterns: [
-		'**/*.{mjs,css,ttf,svg,eot,woff,woff2,html,png,json}'
-	],
-	swDest: '.output/public/sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+  globDirectory: "dist",
+  globPatterns: ["**/*.{mjs,css,ttf,svg,eot,woff,woff2,html,png,json}"],
+  swDest: "dist/sw.js",
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 };
