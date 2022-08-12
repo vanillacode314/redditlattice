df32d9295af6db2db3b3d2c5eda559527b085a6a
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-08-12 19:59:30 +0530

    update service worker register script

--- public/registerSW.js
+++ public/registerSW.js
@@ -1,3 +1,5 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js", { scope: "/" });
-}
+window.addEventListener("load", function () {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js", { scope: "/" });
+  }
+});
