2e9e6cf6388f8ae4c163715bb3cc34509e43cc07
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-10-07 20:12:05 +0530

    fix: image cache options

--- workbox-config.js
+++ workbox-config.js
@@ -1,16 +1,22 @@
-module.exports = {
+/** @type {Parameters<import('workbox-build').generateSW>[0]} */
+const options = {
   globDirectory: "dist",
   globPatterns: ["**/*.{mjs,css,ttf,svg,eot,woff,woff2,html,png,json}"],
   swDest: "dist/sw.js",
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
   runtimeCaching: [
     {
-      urlPattern: ({ request, url }) =>
-        url.origin === "https://redditlattice-server-production.up.railway.app",
+      urlPattern: ({ request }) => request.destination === "image",
       handler: "CacheFirst",
       options: {
-        cacheName: "images-cache",
+        cacheName: "images-assets",
+        expiration: {
+          maxEntries: 250,
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+        },
       },
     },
   ],
 };
+
+module.exports = options;
