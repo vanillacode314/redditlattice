81f5f50c1ffb1ad20dc9bc70d94f2809fff2e0f9
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-10-07 19:27:53 +0530

    feat: runtime image caching using workbox

--- workbox-config.js
+++ workbox-config.js
@@ -3,4 +3,11 @@ module.exports = {
   globPatterns: ["**/*.{mjs,css,ttf,svg,eot,woff,woff2,html,png,json}"],
   swDest: "dist/sw.js",
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+  runtimeCaching: [
+    {
+      urlPattern: ({ request, url }) =>
+        url.origin === "https://redditlattice-server-production.up.railway.app",
+      handler: "CacheFirst",
+    },
+  ],
 };
