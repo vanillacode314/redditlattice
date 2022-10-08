fc0650b5c0ec775e590c2b20bad480cd187ecb82
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-10-08 19:57:11 +0530

    implement lru cache

--- sw.js
+++ sw.js
@@ -1,11 +1,133 @@
 importScripts("/third_party/workbox-v6.5.4/workbox-sw.js");
 importScripts("/third_party/idb-keyval.js");
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.previous = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+      previous: null,
+    };
+    this.length = 1;
+    this.tail = this.head;
+  }
+
+  append(value) {
+    let newNode = new Node(value);
+
+    this.tail.next = newNode;
+    newNode.previous = this.tail;
+    this.tail = newNode;
+
+    this.length++;
+    return newNode;
+  }
+
+  prepend(value) {
+    let newNode = new Node(value);
+
+    newNode.next = this.head;
+    this.head.previous = newNode;
+    this.head = newNode;
+
+    this.length++;
+    return newNode;
+  }
+
+  insert(index, value) {
+    if (!Number.isInteger(index) || index < 0 || index > this.length + 1) {
+      console.error(`Invalid index. Current length is ${this.length}.`);
+      return this;
+    }
+
+    if (index === 0) {
+      this.prepend(value);
+      return this;
+    }
+
+    if (index === this.length) {
+      this.append(value);
+      return this;
+    }
+
+    let newNode = new Node(value);
+    let previousNode = this.head;
+
+    for (let k = 0; k < index - 1; k++) {
+      previousNode = previousNode.next;
+    }
+
+    let nextNode = previousNode.next;
+
+    newNode.next = nextNode;
+    previousNode.next = newNode;
+    newNode.previous = previousNode;
+    nextNode.previous = newNode;
+
+    this.length++;
+  }
+
+  pop(index) {
+    if (!Number.isInteger(index) || index < 0 || index > this.length) {
+      console.error(`Invalid index. Current length is ${this.length}.`);
+      return this;
+    }
+
+    // Remove head
+    if (index === 0) {
+      this.head = this.head.next;
+      this.head.previous = null;
+
+      this.length--;
+      return this;
+    }
+
+    // Remove tail
+    if (index === this.length - 1) {
+      this.tail = this.tail.previous;
+      this.tail.next = null;
+
+      this.length--;
+      return this;
+    }
+
+    // Remove node at an index
+    let previousNode = this.head;
+
+    for (let k = 0; k < index - 1; k++) {
+      previousNode = previousNode.next;
+    }
+    let deleteNode = previousNode.next;
+    let nextNode = deleteNode.next;
+
+    previousNode.next = nextNode;
+    nextNode.previous = previousNode;
+
+    this.length--;
+    return;
+  }
+
+  remove(node) {
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+    this.length--;
+  }
+}
+
 workbox.setConfig({
   modulePathPrefix: "/third_party/workbox-v6.5.4/",
 });
 
 const IDB_LRU_CACHE_KEY = "image-assets";
+const SHIFT_QUEUE = [];
 
 workbox.routing.registerRoute(
   ({ request, url }) =>
@@ -21,16 +143,21 @@ workbox.routing.registerRoute(
         cacheDidUpdate: async ({ request, response }) => {
           /* console.log("SERVICE-WORKER:WillUpdate", { request, response }); */
           await idbKeyval.update(IDB_LRU_CACHE_KEY, (cacheDb) => {
-            cacheDb = cacheDb || { urls: [], limit: 500 };
-            while (cacheDb.urls.length + 1 > cacheDb.limit) {
-              const key = cacheDb.urls.unshift();
+            cacheDb = cacheDb || {
+              urls: new DoublyLinkedList(),
+              map: new Map(),
+              limit: 500,
+            };
+            if (cacheDb.urls.length + 1 > cacheDb.limit) {
+              const url = cacheDb.urls.tail.value;
+              cacheDb.urls.remove(cacheDb.urls.length - 1);
               if (key)
                 caches.open("images-assets").then((cache) => {
-                  cache.delete(key);
+                  cache.delete(url);
                 });
             }
-            if (!cacheDb.urls.includes(request.url))
-              cacheDb.urls.push(request.url);
+            const node = cacheDb.urls.append(request.url);
+            cacheDb.map.set(request.url, node);
             return cacheDb;
           });
           return response;
@@ -38,9 +165,16 @@ workbox.routing.registerRoute(
         cachedResponseWillBeUsed: async ({ request, response }) => {
           /* console.log("SERVICE-WORKER:WillBeUsed", { request, response }); */
           await idbKeyval.update(IDB_LRU_CACHE_KEY, (cacheDb) => {
-            cacheDb = cacheDb || { urls: [], limit: 500 };
-            cacheDb.urls = cacheDb.urls.filter((url) => url != request.url);
-            cacheDb.urls.push(request.url);
+            cacheDb = cacheDb || {
+              urls: new DoublyLinkedList(),
+              map: new Map(),
+              limit: 500,
+            };
+            const node = cacheDb.map.get(request.url);
+            if (node) {
+              cacheDb.urls.remove(node);
+              cacheDb.urls.append(node);
+            }
             return cacheDb;
           });
           return response;
