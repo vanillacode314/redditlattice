2c8907e141271c4295a841aa252b163f22d70e77
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-10-17 13:18:48 +0530

    fix: masonry delete items logic

--- packages/ui/Masonry.tsx
+++ packages/ui/Masonry.tsx
@@ -6,11 +6,12 @@ import {
   createMemo,
   createEffect,
   on,
+  For,
 } from 'solid-js'
 import { createStore } from 'solid-js/store'
 import { Entries, Key } from '@solid-primitives/keyed'
 import { createElementSize } from '@solid-primitives/resize-observer'
-import _ from 'lodash'
+import _, { range } from 'lodash'
 
 interface Item<T = any> {
   id: string
@@ -36,29 +37,46 @@ export const Masonry: Component<Props> = (props) => {
   )
   const [columns, setColumns] = createStore<Record<number, I[]>>()
 
+  function getShortestColumnIndex(): number {
+    let minIndex = 0
+    let minHeight = Infinity
+    for (const x of range(cols())) {
+      const colEl = document.getElementById(`__masonry-col-${x}`)
+      const { height } = colEl ? colEl.getBoundingClientRect() : { height: 0 }
+      if (height <= minHeight) {
+        minIndex = x
+        minHeight = height
+      }
+    }
+    return minIndex
+  }
+
   createEffect(
     on(
       () => props.items,
       (n, p) => {
-        const deletedItems = (p || []).filter(
-          (i) => !(n || []).some((j) => j.id === i.id)
-        )
+        p = p || []
+        const deletedItems = _.difference(p, n)
+        const addedItems = _.difference(n, p)
+        for (const [idx, items] of Object.entries(columns)) {
+          setColumns({
+            [idx]: items.filter((item) =>
+              deletedItems.some((i) => item.id === i.id)
+            ),
+          })
+        }
 
-        const addedItems = n.filter(
-          (i) => !(p || []).some((j) => j.id === i.id)
-        )
-        for (const [col, items] of Object.entries(columns)) {
-          for (const [idx, item] of items.entries()) {
-            if (deletedItems.some((i) => i.id === item.id)) items.splice(idx, 1)
-          }
-        }
-        for (const [idx, chunk] of _.chunk(
-          addedItems,
-          Math.floor(addedItems.length / cols())
-        ).entries()) {
+        let len = addedItems.length
+        let i = 0
+        requestAnimationFrame(function handler() {
+          if (i === len) return
+          const item = addedItems[i]
+          const idx = getShortestColumnIndex()
           const old = columns[idx] || []
-          setColumns({ [idx]: [...old, ...chunk] })
-        }
+          setColumns({ [idx]: [...old, item] })
+          i++
+          requestAnimationFrame(handler)
+        })
       }
     )
   )
@@ -77,18 +95,24 @@ export const Masonry: Component<Props> = (props) => {
   return (
     <div
       class="grid"
-      style={{ 'grid-template-columns': `repeat(${cols()},1fr)` }}
+      style={{
+        'grid-template-columns': `repeat(${cols()},1fr)`,
+        'align-items': 'start',
+      }}
       ref={setEl}
     >
-      <Entries of={columns}>
-        {(key, item) => (
-          <div class="flex flex-col">
-            <Key each={item()} by="id">
-              {(item) => props.children(item().id, item().data, colWidth)}
-            </Key>
-          </div>
-        )}
-      </Entries>
+      <Key each={Object.entries(columns)} by={([, v]) => v}>
+        {(value) => {
+          const [idx, items] = value()
+          return (
+            <div class="flex flex-col" id={`__masonry-col-${idx}`}>
+              <Key each={items} by="id">
+                {(item) => props.children(item().id, item().data, colWidth)}
+              </Key>
+            </div>
+          )
+        }}
+      </Key>
     </div>
   )
 }
