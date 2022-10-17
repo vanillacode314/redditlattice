84633f9f9a9168adb1d90ffa21c9483414199b54
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-10-17 18:29:37 +0530

    refactor: inline service worker registration

--- apps/web/src/entry-client.tsx
+++ apps/web/src/entry-client.tsx
@@ -1,7 +1,3 @@
 import { mount, StartClient } from 'solid-start/entry-client'
 
 mount(() => <StartClient />, document)
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js', { scope: '/' })
-}
