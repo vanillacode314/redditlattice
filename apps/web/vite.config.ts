72b46c5603932fcb2ce0f6adc65cb14265933c71
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-10-20 01:27:58 +0530

    trigger deploy

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     },
   },
   plugins: [
-    solid({ ssr: false, adapter: netlify() }),
+    solid({ ssr: false, adapter: netlify({ edge: true }) }),
     Unocss({
       presets: [
         presetAttributify(),
