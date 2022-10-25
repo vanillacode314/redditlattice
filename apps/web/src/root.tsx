b5e46a9f350889ebb19a21c7f08944a7bb65f7cb
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-10-25 13:38:08 +0530

    viewport-fit cover

--- apps/web/src/root.tsx
+++ apps/web/src/root.tsx
@@ -61,7 +61,10 @@ export const Root: Component = () => {
             : `RedditLattice`}
         </Title>
         <Meta charset="utf-8" />
-        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
         <Link rel="manifest" href="/manifest.webmanifest"></Link>
         <Link rel="icon" href="/favicon.svg" />
       </Head>
