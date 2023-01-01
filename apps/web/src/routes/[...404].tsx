62387dbcf105a24d64da17a43e5018301fef84f5
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2023-01-01 19:43:14 +0530

    add statuscode to 404 page

--- apps/web/src/routes/[...404].tsx
+++ apps/web/src/routes/[...404].tsx
@@ -1,5 +1,11 @@
 import { Navigate } from 'solid-start'
+import { HttpStatusCode } from 'solid-start/server'
 
 export default function Home() {
-  return <Navigate href="/" />
+  return (
+    <>
+      <HttpStatusCode code={404} />
+      <Navigate href="/" />
+    </>
+  )
 }
