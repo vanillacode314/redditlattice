3f4048ffad498a2de6e5f1da2cee677c32c7aec3
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2023-04-23 20:44:56 +0530

    fix infinite loading firstload logic

--- packages/ui/InfiniteLoading.tsx
+++ packages/ui/InfiniteLoading.tsx
@@ -6,8 +6,11 @@ import {
   createSignal,
   JSXElement,
   mergeProps,
+  on,
+  Show,
   untrack,
 } from 'solid-js'
+import { isServer } from 'solid-js/web'
 export type State = 'idle' | 'error' | 'loading' | 'completed'
 export type InfiniteHandler = (
   setState: (state: State) => void,
@@ -24,43 +27,63 @@ interface Props {
   onInfinite: InfiniteHandler
 }
 
-export const InfiniteLoading: Component<Props> = (props: Props) => {
+const InfiniteLoadingInner: Component<Props> = (props: Props) => {
   let el!: HTMLDivElement
 
-  const merged = mergeProps(
-    { key: '', distance: 0, target: 'body', firstload: true },
-    props
-  )
-
+  const merged = mergeProps({ key: '', distance: 0, firstload: true }, props)
   const { onInfinite } = props
-
   const [state, setState] = createSignal<State>('idle')
-  const target = createMemo<HTMLElement>(() => {
-    if (merged.target instanceof HTMLElement) {
-      return merged.target
-    } else {
-      return document.querySelector(merged.target) as HTMLElement
-    }
-  })
-  const useVisibilityObserver = createVisibilityObserver({
-    threshold: 0.8,
-    root: target(),
-    rootMargin: `0px 0px ${merged.distance}px 0px`,
-  })
-
-  const visible = useVisibilityObserver(() => el)
-
+  const [target, setTarget] = createSignal<HTMLElement | null>(null)
   createEffect(() => {
-    if (!visible()) return
-    untrack(() => {
-      if (state() !== 'idle') return
-      load()
-    })
+    if (merged.target instanceof HTMLElement) {
+      setTarget(merged.target)
+      return
+    }
+    setTarget(
+      merged.target
+        ? (document.querySelector(merged.target) as HTMLElement)
+        : null
+    )
   })
+
+  const [visible, setVisible] = createSignal<boolean>(false)
+  createEffect(() => {
+    new IntersectionObserver(
+      (entries) => setVisible(entries.some((entry) => entry.isIntersecting)),
+      {
+        threshold: 0.8,
+        root: target(),
+        rootMargin: `0px 0px ${merged.distance}px 0px`,
+      }
+    ).observe(el)
+  })
+
+  let isFirstLoad: boolean = true
+  createEffect(
+    on(
+      visible,
+      (visible) => {
+        if (isFirstLoad && visible) {
+          isFirstLoad = false
+          return
+        }
+        if (!visible) return
+        untrack(() => {
+          if (state() !== 'idle') return
+          load()
+        })
+      },
+      { defer: true }
+    )
+  )
 
   const updateState = (newState: State) => {
     setState(newState)
-    if (visible() && state() == 'idle') load()
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        if (visible() && state() == 'idle') load()
+      })
+    })
   }
 
   const load = (firstload: boolean = false) => {
@@ -68,14 +91,19 @@ export const InfiniteLoading: Component<Props> = (props: Props) => {
     onInfinite(updateState, firstload, load)
   }
 
-  const setup = () => load(merged.firstload)
-
   createEffect(() => {
     merged.key
-    untrack(setup)
+    if (merged.firstload) load(true)
   })
 
   return <div ref={el}>{props.children(state(), load)}</div>
 }
 
+export const InfiniteLoading: Component<Props> = (props: Props) => {
+  return (
+    <Show when={!isServer}>
+      <InfiniteLoadingInner {...props} />
+    </Show>
+  )
+}
 export default InfiniteLoading
